fcb15a369c31ce767b885f4e715dd5b41d2430cc
Sergey Chernikov <Sergey _Chernikov@havi.com>
2022-05-20 16:31:06 +0600

    #Task 2
    add webpack configuration for react
    add environment configuration for build dev/prod
    add prettier

---
+++ react-movie-catalog-app/src/components/Application.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Application = () => {
+    return (
+        <div>
+            <h1>Hello From webpack react app</h1>
+        </div>
+    )
+}
---
+++ react-movie-catalog-app/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Application } from "./components/Application"
+import './assets/css/index.css'
+
+const rootTag = document.getElementById("rootTag")
+ReactDOM.render(<Application />, rootTag)
