36f9583f6578e9e74f82e5f475e1ff835e6fb243
Sergey Chernikov <Sergey _Chernikov@havi.com>
2022-07-21 13:14:31 +0600

    add MovieOperation  snapshot test

---
+++ react-movie-catalog-app/src/test/MovieOperation.snapshot.test.tsx
@@ -0,0 +1,12 @@
+import  React from 'react'
+import { MovieOperation } from '../components/MovieOperation'
+import { render, cleanup } from '@testing-library/react'
+
+afterEach(cleanup)
+
+describe("MovieOperation Snapshot Test Suit", () => {
+  test('renders correctly', () => {
+    const { asFragment } = render(<MovieOperation   movie={null} operationHandler={ () => console.log("Hi from Jest")} closeWindow={() => console.log("Hi from Jest")}/>)
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
