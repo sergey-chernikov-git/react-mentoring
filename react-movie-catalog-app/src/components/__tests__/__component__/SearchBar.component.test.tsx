249257aaae95973b82a5e0d23bcfe11ce8f755df
Sergey Chernikov <Sergey _Chernikov@havi.com>
2022-07-22 18:54:18 +0600

    extended test

--- react-movie-catalog-app/src/components/__tests__/__component__/SearchBar.component.test.tsx
+++ react-movie-catalog-app/src/components/__tests__/__component__/SearchBar.component.test.tsx
@@ -6,6 +6,9 @@ import {BrowserRouter, MemoryRouter} from 'react-router-dom'
 import { expect } from '@jest/globals';
 import configureStore from 'redux-mock-store';
 
+import '@testing-library/jest-dom'
+import '@testing-library/jest-dom/extend-expect'
+
 const mockStore = configureStore([]);
 
 afterEach(cleanup);
@@ -18,20 +21,30 @@ describe('SearchBar Component Test Suit', () => {
     });
   });
 
-
   const searchText = 'Test Movie';
   
+  
+  // test('Search Input Rendering ', () => {     
+  //   const route = '/'
+  //   render(
+  //     <MemoryRouter initialEntries={[route]}>
+  //       <Provider store={store}>
+  //         <SearchBar searchQuery={searchText} />
+  //       </Provider>
+  //     </MemoryRouter>);
+  //   const searchElement = screen.getByTestId("search-input-value");
+  //   expect(searchElement).toBeInTheDocument()
+  // });
 
-  // const searchInput = screen.getByTestId("search-input-value");
-
-  test('Search Input Rendering ', () => {
-   const route = '/'
-   render(
-    <MemoryRouter initialEntries={[route]}>
-      <Provider store={store}>
-        <SearchBar searchQuery={searchText} />
-      </Provider>
-    </MemoryRouter>);
-    expect(screen.getByTestId("search-input-value")).not.toBeNull()
-  });
+  test(`Search Input Text to be ${searchText} `, () => {
+    const route = '/'
+    render(
+      <MemoryRouter initialEntries={[route]}>
+        <Provider store={store}>
+          <SearchBar searchQuery={searchText} />
+        </Provider>
+      </MemoryRouter>);
+     const searchElement = screen.getByTestId("search-input-value");
+     expect((searchElement as HTMLInputElement).value).toEqual(searchText)
+   });
 });
